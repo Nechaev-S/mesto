a06a9cdbdae57af1ef9606aee17b37bedf740870
Sergey Nechaev <s4079808@yandex.ru>
2021-07-21 02:36:20 +0300

    добавил js к попапу

--- scripts/index.js
+++ scripts/index.js
@@ -4,40 +4,29 @@ const saveButton = document.querySelector(".popup__save-button");
 const popup = document.querySelector(".popup");
 
 function toggLePopup() {
-    popup.classList.toggle("popup_is-opened");
+  popup.classList.toggle("popup_is-opened");
 }
 
 popupButton.addEventListener("click", toggLePopup);
 closeButton.addEventListener("click", toggLePopup);
 
-// function popupOpen() {
-//     editPopup.classList.add("popup_opened")
-//   }
-
 function popupClose() {
-    popup.classList.remove("popup_is-opened")
-  }
-
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__text_check_name");
-let jobInput = document.querySelector(".popup__text_check_job");
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-
-    let nameInput = document.querySelector(".popup__text_check_name");
-    let jobInput = document.querySelector(".popup__text_check_job");
-    let profileName = document.querySelector(".profile__name");
-    let profileJob = document.querySelector(".profile__job");
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-
-    popupClose();
+  popup.classList.remove("popup_is-opened")
 }
 
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__text_check_name");
+const jobInput = document.querySelector(".popup__text_check_job");
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  const nameInput = document.querySelector(".popup__text_check_name");
+  const jobInput = document.querySelector(".popup__text_check_job");
+  const profileName = document.querySelector(".profile__name");
+  const profileJob = document.querySelector(".profile__job");
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupClose();
+}
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
-saveButton.addEventListener('click', formSubmitHandler)
+formElement.addEventListener('submit', formSubmitHandler);
